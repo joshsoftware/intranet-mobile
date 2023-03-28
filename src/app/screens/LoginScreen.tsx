dc2fac4d87b8ea0203fdb47b8f097ae090d476b9
Chetan Satpute <chetan.satpute@joshsoftware.com>
2023-03-28 16:16:19 +0530

    Added Google Authentication
    Without UI, Navigation and Backend Integration

--- src/app/screens/LoginScreen.tsx
+++ src/app/screens/LoginScreen.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {Button, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+
+import {WEB_CLIENT_ID} from '@env';
 
 const LoginScreen = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: WEB_CLIENT_ID,
+    });
+  }, []);
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+
+      console.log(userInfo);
+    } catch (error: any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+
+      console.log(error);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.text}>Login Screen</Text>
+      <Button title="Login With Google" onPress={signIn} />
     </SafeAreaView>
   );
 };
