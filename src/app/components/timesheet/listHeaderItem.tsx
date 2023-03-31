ee55d0a69ca9fef0dde07065042270079f1233ce
Saurabh Nagre <saurabh.nagre@joshsoftware.com>
2023-03-31 18:37:13 +0530

    Added Manager and timesheet list screens

---
+++ src/app/components/timesheet/listHeaderItem.tsx
@@ -0,0 +1,33 @@
+import React, {memo} from 'react';
+import {TextStyle, View, ViewStyle} from 'react-native';
+
+import Typography from '../typography';
+
+import {flexStyles} from '../../../styles';
+
+type Props = {
+  style?: ViewStyle;
+  headerTextStyle?: TextStyle;
+  descTextStyle?: TextStyle;
+  label: string;
+  value: string;
+};
+
+const ListHeaderItem = ({
+  style,
+  headerTextStyle,
+  descTextStyle,
+  label,
+  value,
+}: Props) => (
+  <View style={[flexStyles.vertical, style]}>
+    <Typography type="description" style={headerTextStyle}>
+      {value}
+    </Typography>
+    <Typography type="description" style={descTextStyle}>
+      {label}
+    </Typography>
+  </View>
+);
+
+export default memo(ListHeaderItem);
