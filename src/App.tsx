cf97ff19226ccc5b2ffaaa3aa1fcbd4575454dd7
Sushant Patil <sushant.patiljoshsoftware.com>
2023-03-31 11:25:34 +0530

    Changes in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import RootNavigator from './app/navigation/RootNavigator';
+import CustomTabView from './app/navigation/customTabView';
 
 const App = () => {
   return (
