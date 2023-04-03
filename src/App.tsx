c728579c539d3e06e878c54f0929099d7f0a093d
Sushant Patil <sushant.patiljoshsoftware.com>
2023-04-03 18:14:09 +0530

    App changed for run

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import RootNavigator from './app/navigation/RootNavigator';
-import CustomTabView from './app/navigation/customTabView';
+import CustomTabView from './app/screens/userProfile/customTabView';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <RootNavigator />
+    // </NavigationContainer>
+    <CustomTabView />
   );
 };
 
