cbfe602b93f6c61e4cc3f438984b720452ef62fd
Chetan Satpute <chetan.satpute@joshsoftware.com>
2023-04-10 02:14:36 +0530

    Added Header to user profile screen

--- src/app/screens/userProfile/index.tsx
+++ src/app/screens/userProfile/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 import CustomTabView from './customTabView';
+import Header from '../../components/Header';
 
 const UserProfile = () => {
-  return <CustomTabView />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header onMainScreen={false} />
+      <CustomTabView />
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default UserProfile;
