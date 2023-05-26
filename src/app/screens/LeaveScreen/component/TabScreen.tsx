9165e33bcfa293b5cdfab5878bd233b4138aed4f
Chetan Satpute <chetan.satpute@joshsoftware.com>
2023-05-26 10:07:19 +0530

    Filter Modal Implementation on leave screen

--- src/app/screens/LeaveScreen/component/TabScreen.tsx
+++ src/app/screens/LeaveScreen/component/TabScreen.tsx
@@ -1,13 +1,26 @@
-import React, {useState} from 'react';
-import {ActivityIndicator, FlatList, StyleSheet, View} from 'react-native';
+import React, {useCallback, useMemo, useState} from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {useQueryClient} from 'react-query';
 
-import Input from '../../../components/input';
 import LeaveListItem from './LeaveListItem';
+import FilterModal from './FilterModal';
 import Typography from '../../../components/typography';
+import Touchable from '../../../components/touchable';
+import DateRange from '../../../components/pickers/dateRange';
 import {useLeaveList} from '../leave.hooks';
 
+import {dateFormate, startOfMonth, todaysDate} from '../../../utils/date';
+import {dateFormater} from '../../../utils/dateFormater';
+
 import colors from '../../../constant/colors';
-import {Search} from '../../../constant/icons';
+import {Calendar, Search} from '../../../constant/icons';
 import {ILeaveFilters} from '../interface';
 
 interface Props {
@@ -15,42 +28,146 @@ interface Props {
 }
 
 function TabScreen({route}: Props) {
+  const queryClient = useQueryClient();
+  const [showFilterModal, setShowFilterModal] = useState(false);
   const [filters, setFilters] = useState<ILeaveFilters>({
-    leave_type: 'LEAVE,WFH',
+    leave_type: '',
     pending_flag: route === 'pending' ? 'true' : 'false',
+    active_or_all_flags: 'active',
+    from: dateFormater(startOfMonth),
+    to: dateFormater(todaysDate),
+    page_no: 0,
   });
 
-  const {data, isLoading} = useLeaveList(filters);
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    refetch,
+    isRefetching,
+    isRefetchError,
+  } = useLeaveList(filters);
+
+  let content: React.ReactNode = null;
 
   if (isLoading) {
-    return (
+    content = (
       <View style={styles.centerContainer}>
         <ActivityIndicator size="large" color={colors.PRIMARY} />
       </View>
     );
-  }
-
-  if (!data || data.length === 0) {
-    return (
+  } else if (isError || isRefetchError) {
+    content = (
+      <ScrollView
+        contentContainerStyle={styles.centerContainer}
+        refreshControl={
+          <RefreshControl refreshing={isRefetching} onRefresh={refetch} />
+        }>
+        <Typography type="error">{error?.message}</Typography>
+      </ScrollView>
+    );
+  } else if (!data) {
+    content = (
       <View style={styles.centerContainer}>
-        <Typography type="error">Could not fetch leaves!</Typography>
+        <Typography type="error">Could not get leaves!</Typography>
       </View>
     );
+  } else if (data.length === 0) {
+    content = (
+      <View style={styles.centerContainer}>
+        <Typography type="secondaryText">No Leaves!</Typography>
+      </View>
+    );
+  } else {
+    content = (
+      <FlatList
+        data={data}
+        refreshControl={
+          <RefreshControl refreshing={isRefetching} onRefresh={refetch} />
+        }
+        renderItem={({item}) => <LeaveListItem {...item} />}
+      />
+    );
   }
+
+  const [isDateRangeVisible, setIsDateRangeVisible] = useState(false);
+
+  const updateFilters = useCallback(
+    (newFilters: ILeaveFilters) => {
+      setFilters(newFilters);
+      queryClient.invalidateQueries(['leave', filters]);
+    },
+    [filters, queryClient],
+  );
+
+  const onDateRangeSubmit = useCallback(
+    (startDate?: Date, endDate?: Date) => {
+      if (startDate && endDate) {
+        updateFilters({
+          ...filters,
+          from: dateFormater(startDate),
+          to: dateFormater(endDate),
+        });
+      } else {
+        updateFilters({
+          ...filters,
+          from: dateFormater(startOfMonth),
+          to: dateFormater(todaysDate),
+        });
+      }
+    },
+    [filters, updateFilters],
+  );
+
+  const toggelDatePicker = () => setIsDateRangeVisible(v => !v);
+
+  const dateRangeText = useMemo(
+    () => `${dateFormate(filters.from)} to ${dateFormate(filters.to)}`,
+    [filters.to, filters.from],
+  );
+
+  const toggleFilterModal = () => {
+    setShowFilterModal(!showFilterModal);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.row}>
         <View style={styles.searchBoxContainer}>
-          <Input StartIcon={Search} placeholder="Search" />
+          <Touchable
+            type="opacity"
+            onPress={toggelDatePicker}
+            activeOpacity={0.5}
+            style={styles.filter}>
+            <Calendar height={17} width={17} />
+            <Typography type={'subheader'} style={styles.filterText}>
+              {dateRangeText}
+            </Typography>
+          </Touchable>
+          <DateRange
+            onSubmit={onDateRangeSubmit}
+            isVisible={isDateRangeVisible}
+            toggleModal={toggelDatePicker}
+            initialStartDateValue={startOfMonth}
+            initialEndDateValue={todaysDate}
+          />
         </View>
-        <View style={styles.filterContainer}>
+        <Touchable
+          type="opacity"
+          style={styles.filterContainer}
+          onPress={toggleFilterModal}>
           <Search />
-        </View>
+        </Touchable>
       </View>
-      <FlatList
-        data={data}
-        renderItem={({item}) => <LeaveListItem {...item} />}
+
+      {content}
+
+      <FilterModal
+        isVisible={showFilterModal}
+        closeModal={toggleFilterModal}
+        filters={filters}
+        setFilter={updateFilters}
       />
     </View>
   );
@@ -77,6 +194,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  filter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingVertical: 10,
+    borderColor: colors.TEXT_INPUT_BORDER,
+    borderBottomWidth: 1,
+  },
+  filterText: {
+    fontSize: 14,
+    padding: 5,
+    paddingHorizontal: 16,
   },
 });
 
--- src/app/screens/LeaveScreen/leave.hooks.ts
+++ src/app/screens/LeaveScreen/leave.hooks.ts
@@ -1,22 +1,39 @@
+import {AxiosError} from 'axios';
 import {useQuery} from 'react-query';
 
 import {
   getLeaveDetailRequest,
   getLeaveListRequest,
 } from '../../services/api/leave';
+import toast from '../../utils/toast';
 
-import {ILeaveFilters} from './interface';
+import {ILeaveFilters, IProjectData, IUserData} from './interface';
 
 export function useLeaveList(filters: ILeaveFilters) {
-  const {data, isLoading, isError} = useQuery({
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    refetch,
+    isRefetching,
+    isRefetchError,
+  } = useQuery({
     queryKey: ['leaveList', filters],
     queryFn: async () => getLeaveListRequest(filters),
+    onError: (err: AxiosError) => {
+      toast(err.message, 'error');
+    },
   });
 
   return {
     data: data?.data.data.leaves,
     isLoading,
     isError,
+    error,
+    refetch,
+    isRefetching,
+    isRefetchError,
   };
 }
 
@@ -31,4 +48,40 @@ export function useLeaveDetail(leaveID: number) {
     isLoading,
     isError,
   };
+}
+
+export function useProjectList() {
+  // TODO: Sample data
+  const data: IProjectData[] = [
+    {
+      name: 'Intranet',
+      project_id: 0,
+    },
+    {
+      name: 'Intern Training',
+      project_id: 1,
+    },
+  ];
+
+  return data.map(({name, project_id}) => ({
+    label: name,
+    value: project_id,
+  }));
+}
+
+export function useUserList() {
+  // TODO: Sample data
+  const data: IUserData[] = [
+    {
+      name: 'Chetan Satpute',
+      email: 'chetan.satpute@joshsoftware.com',
+      user_id: 929,
+      emp_id: 'JIN0105',
+    },
+  ];
+
+  return data.map(({name, user_id}) => ({
+    label: name,
+    value: user_id,
+  }));
 }
