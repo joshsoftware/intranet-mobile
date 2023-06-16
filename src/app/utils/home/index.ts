05f87457b5c75bf99eeb938e419b712148c8238e
Chetan Satpute <chetan.satpute@joshsoftware.com>
2023-06-16 13:38:01 +0530

    Handle multiple date types on single day

--- src/app/utils/home/index.ts
+++ src/app/utils/home/index.ts
@@ -1,33 +1,18 @@
 import colors from '../../constant/colors';
 
-const dateTypes = {
-  filled: {
-    color: colors.LIGHT_GREEN_BACKGROUND,
-    type: 'filled',
-  },
-  notFilled: {
-    color: colors.LIGHT_RED_BACKGROUND,
-    type: 'unfilled',
-  },
-  incompleteFilled: {
-    color: colors.YELLOW_BACKGROUND,
-    type: 'partiallyFilled',
-  },
-  leaves: {
-    color: colors.LIGHT_BLUE_BACKGROUND,
-    type: 'leave',
-  },
-  holidays: {
-    color: colors.GRAY_BACKGROUND,
-    type: 'holiday',
-  },
+const dateTypes: Record<string, string> = {
+  filled: colors.LIGHT_GREEN_BACKGROUND,
+  notFilled: colors.LIGHT_RED_BACKGROUND,
+  incompleteFilled: colors.YELLOW_BACKGROUND,
+  leaves: colors.LIGHT_BLUE_BACKGROUND,
+  holidays: colors.GRAY_BACKGROUND,
 };
 
 export const generateMarkedDates = (data: Record<string, string[]>) => {
   let result: Record<string, any> = {};
 
-  Object.entries(dateTypes).forEach(([dateType, value]) => {
-    const {color, type} = value;
+  ['holidays', 'leaves'].forEach(dateType => {
+    const color = dateTypes[dateType];
 
     data[dateType].forEach((date: string) => {
       result[date] = {
@@ -38,7 +23,28 @@ export const generateMarkedDates = (data: Record<string, string[]>) => {
             alignItems: 'center',
           },
         },
-        type,
+        type: dateType,
+      };
+    });
+  });
+
+  ['filled', 'notFilled', 'incompleteFilled'].forEach(dateType => {
+    const color = dateTypes[dateType];
+
+    data[dateType].forEach((date: string) => {
+      result[date] = {
+        customStyles: {
+          container: {
+            borderWidth: result[date] ? 4 : 0,
+            borderColor: result[date]
+              ? dateTypes[result[date].type]
+              : 'transparent',
+            backgroundColor: color,
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        },
+        type: dateType,
       };
     });
   });
