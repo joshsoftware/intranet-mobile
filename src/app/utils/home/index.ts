1b9e82a92f3bc608255f7640cba5a38f71fb3344
Chetan Satpute <chetan.satpute@joshsoftware.com>
2023-06-16 15:47:26 +0530

    Fixed last errors in implementation

--- src/app/utils/home/index.ts
+++ src/app/utils/home/index.ts
@@ -1,20 +1,35 @@
 import colors from '../../constant/colors';
 
-const dateTypes: Record<string, string> = {
-  filled: colors.LIGHT_GREEN_BACKGROUND,
-  notFilled: colors.LIGHT_RED_BACKGROUND,
-  incompleteFilled: colors.YELLOW_BACKGROUND,
-  leaves: colors.LIGHT_BLUE_BACKGROUND,
-  holidays: colors.GRAY_BACKGROUND,
+const dateTypes: Record<string, {color: string; type: string}> = {
+  filled: {
+    color: colors.LIGHT_GREEN_BACKGROUND,
+    type: 'filled',
+  },
+  notFilled: {
+    color: colors.LIGHT_RED_BACKGROUND,
+    type: 'unfilled',
+  },
+  incompleteFilled: {
+    color: colors.YELLOW_BACKGROUND,
+    type: 'partiallyFilled',
+  },
+  leaves: {
+    color: colors.LIGHT_BLUE_BACKGROUND,
+    type: 'leave',
+  },
+  holidays: {
+    color: colors.GRAY_BACKGROUND,
+    type: 'holiday',
+  },
 };
 
 export const generateMarkedDates = (data: Record<string, string[]>) => {
   let result: Record<string, any> = {};
 
   ['holidays', 'leaves'].forEach(dateType => {
-    const color = dateTypes[dateType];
+    const {color, type} = dateTypes[dateType];
 
-    data[dateType].forEach((date: string) => {
+    data[dateType].forEach(date => {
       result[date] = {
         customStyles: {
           container: {
@@ -23,31 +38,52 @@ export const generateMarkedDates = (data: Record<string, string[]>) => {
             alignItems: 'center',
           },
         },
-        type: dateType,
+        type,
       };
     });
   });
 
   ['filled', 'notFilled', 'incompleteFilled'].forEach(dateType => {
-    const color = dateTypes[dateType];
+    const {color, type} = dateTypes[dateType];
 
-    data[dateType].forEach((date: string) => {
+    data[dateType].forEach(date => {
+      let borderColor = 'transparent';
+      let borderWidth = 0;
+
+      if (result[date]) {
+        borderWidth = 4;
+
+        switch (result[date].type) {
+          case 'leave':
+            borderColor = dateTypes.leaves.color;
+            break;
+          case 'holiday':
+            borderColor = dateTypes.holidays.color;
+            break;
+
+          default:
+            break;
+        }
+      }
+
       result[date] = {
         customStyles: {
           container: {
-            borderWidth: result[date] ? 4 : 0,
-            borderColor: result[date]
-              ? dateTypes[result[date].type]
-              : 'transparent',
+            borderWidth: borderWidth,
+            borderColor: borderColor,
             backgroundColor: color,
             justifyContent: 'center',
             alignItems: 'center',
           },
         },
-        type: dateType,
+        type,
       };
     });
   });
 
   return result;
+};
+
+export const filterWFHFromLeaves = (data: {leave_type: string}[]) => {
+  return data.filter(({leave_type}) => leave_type !== 'WFH');
 };
