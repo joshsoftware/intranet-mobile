bdc2180edbf1802175e76cc1e4e9847cb6d69dbf
amar-josh <amar.jadhav@joshsoftware.com>
2024-07-08 14:44:35 +0530

    Add demo code into files to avoid page breaks

--- src/app/screens/PeerlyScreens/HomeScreen/index.tsx
+++ src/app/screens/PeerlyScreens/HomeScreen/index.tsx
@@ -1,6 +1,8 @@
 import {View} from 'react-native';
+import {loginPeerlySystem} from './home.hooks';
 
 function PeerlyHomeScreen() {
+  const {} = loginPeerlySystem();
   return <View>PeerlyHomeScreen</View>;
 }
 
