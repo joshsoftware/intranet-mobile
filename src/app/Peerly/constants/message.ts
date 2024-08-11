92ed27ec0223a82f21c0389331a2e58ead4264bc
amar-josh <amar.jadhav@joshsoftware.com>
2024-08-11 17:10:40 +0530

    Add message constant file

---
+++ src/app/Peerly/constants/message.ts
@@ -0,0 +1,5 @@
+export default {
+  SELECT_COWORKER_NAME: 'Please select the co-worker name.',
+  SELECT_CORE_VALUE: 'Please select the core Value.',
+  ENTER_DESCIPTION: 'Please enter Description.',
+};
